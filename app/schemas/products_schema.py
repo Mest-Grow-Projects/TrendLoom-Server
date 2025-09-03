b3a4351f289f06e4bf55f231eba01ef12a3fdf7f
george-asiedu <asiedug41@gmail.com>
2025-09-03 18:37:19 +0000

    product schemas defined

---
+++ app/schemas/products_schema.py
@@ -0,0 +1,61 @@
+from datetime import datetime
+from pydantic import BaseModel, Field
+from app.core.constants import validations, patterns_regex
+from typing import List
+from app.models.products import Attributes, StockStatus
+
+
+class ProductsSchema(BaseModel):
+    name: str = Field(
+        min_length=2,
+        max_length=50,
+        pattern=patterns_regex["name"],
+        description=validations["name"],
+    )
+    description: str = Field(min_length=2)
+    price: float
+
+    model_config = {"extra": "forbid"}
+
+
+class UpdateProductSchema(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    images: List[str] | None = None
+    attributes: List[Attributes] | None = None
+    features: List[str] | None = None
+    specifications: List[str] | None = None
+    stockStatus: StockStatus | None = None
+    is_featured: bool | None = None
+    is_new: bool | None = None
+    rating: float | None = None
+
+    model_config = {"extra": "forbid"}
+
+
+class ProductOut(BaseModel):
+    id: str
+    name: str
+    description: str
+    price: float
+    images: List[str] | None = None
+    attributes: List[Attributes] | None = None
+    features: List[str] | None = None
+    specifications: List[str] | None = None
+    stockStatus: StockStatus
+    is_featured: bool | None = None
+    is_new: bool | None = None
+    rating: float | None = None
+    createdAt: datetime
+    updatedAt: datetime
+
+
+class ProductResponse(BaseModel):
+    message: str
+    data: ProductOut
+
+
+class ProductsResponse(BaseModel):
+    message: str
+    data: List[ProductOut]
