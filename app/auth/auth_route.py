06ab2b94d5a3f939b66c90d72b469c051ab03ba5
george-asiedu <asiedug41@gmail.com>
2025-09-03 12:13:52 +0000

    response models added to routes

--- app/auth/auth_route.py
+++ app/auth/auth_route.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter, status
 from .auth_service import auth_service
-from app.schemas.auth_schema import SignupSchema, LoginSchema, VerifyAccount
+from app.schemas.auth_schema import (
+    SignupSchema,
+    LoginSchema,
+    VerifyAccount,
+    SignupResponse,
+    LoginResponse,
+    VerifyAccountResponse
+)
 
 router = APIRouter(
     prefix="/auth",
@@ -11,22 +18,25 @@ router = APIRouter(
     "/signup",
     status_code=status.HTTP_201_CREATED,
     summary="Register a new user",
+    response_model=SignupResponse
 )
 async def signup_user(user: SignupSchema):
     return await auth_service.signup(user)
 
 @router.post(
-    "/login",
-    status_code=status.HTTP_200_OK,
-    summary="Login a user",
-)
-async def login_user(user: LoginSchema):
-    return await auth_service.login(user)
-
-@router.post(
     "/verify-account",
     status_code=status.HTTP_200_OK,
     summary="Verify a user's account with code",
+    response_model=VerifyAccountResponse,
 )
-async def verify_user(data: VerifyAccount):
-    return await auth_service.verify(data.code)
+async def verify_user(data: VerifyAccount, token: str):
+    return await auth_service.verify(data.code, token)
+
+@router.post(
+    "/login",
+    status_code=status.HTTP_200_OK,
+    summary="Login a user",
+    response_model=LoginResponse
+)
+async def login_user(user: LoginSchema):
+    return await auth_service.login(user)
