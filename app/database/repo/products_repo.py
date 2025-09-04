b2befe500953bfc07ad35ce2e156e80c93675da3
george-asiedu <asiedug41@gmail.com>
2025-09-04 14:46:44 +0000

    get_products_by_ids function implemented to fetch products with ids

--- app/database/repo/products_repo.py
+++ app/database/repo/products_repo.py
@@ -1,8 +1,10 @@
+from typing import List
 from app.models.cart import Cart
 from app.models.products import Product
 from fastapi import HTTPException, status
 from app.core.constants import status_messages
 from beanie import PydanticObjectId
+from beanie.operators import In
 
 async def check_existing_product(name: str) -> bool:
     existing_product = await Product.find_one(Product.name == name)
@@ -23,8 +25,12 @@ async def find_product_by_id(product_id: str) -> Product:
     return product
 
 async def get_or_create_cart(user_id: PydanticObjectId) -> Cart:
-    cart = await Cart.get(user_id)
+    cart = await Cart.find_one(Cart.user_id == user_id)
     if not cart:
         cart = Cart(user_id=user_id ,items=[])
         await cart.insert()
     return cart
+
+async def get_products_by_ids(product_ids: List[PydanticObjectId]) -> List[Product]:
+    products = await Product.find(In(Product.id, product_ids)).to_list()
+    return products
