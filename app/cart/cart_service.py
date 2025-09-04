343538ee7cfaf651b70616a5a8c362fa79fb1b13
george-asiedu <asiedug41@gmail.com>
2025-09-04 14:45:25 +0000

    get from cart functionality implemented to get all products in a cart

--- app/cart/cart_service.py
+++ app/cart/cart_service.py
@@ -1,7 +1,11 @@
 from beanie import PydanticObjectId
 from app.core.constants import success_messages
 from app.models.cart import CartItem
-from app.database.repo.products_repo import find_product_by_id, get_or_create_cart
+from app.database.repo.products_repo import (
+    find_product_by_id,
+    get_or_create_cart,
+    get_products_by_ids,
+)
 from app.database.repo.user_repo import find_user_by_id
 from app.schemas.cart_schema import AddToCartRequest
 
@@ -18,7 +22,7 @@ class CartService:
 
         cart_item_exists = False
         for item in cart.items:
-            if item.product.id == data.product_id:
+            if item.product_id == data.product_id:
                 item.quantity += data.quantity
                 cart_item_exists = True
                 break
@@ -30,6 +34,50 @@ class CartService:
         return {
             "message": success_messages["cart_added"],
             "data": cart
+        }
+
+
+    async def remove_from_cart(self, user_id: PydanticObjectId, product_id: PydanticObjectId):
+        cart = await get_or_create_cart(user_id)
+        cart.items = [item for item in cart.items if item.product_id != product_id]
+        await cart.save()
+        return cart
+
+
+    async def get_from_cart(self, user_id: PydanticObjectId):
+        cart = await get_or_create_cart(user_id)
+        if not cart.items:
+            return {
+                "items": [],
+                "total_price": 0.0,
+                "user_id": cart.user_id
+            }
+
+        product_ids = [item.product_id for item in cart.items]
+        products = await get_products_by_ids(product_ids)
+        product_map = { product.id: product for product in products }
+
+        detailed_items = []
+        total_price = 0.0
+
+        for item in cart.items:
+            product_details = product_map.get(item.product_id)
+            if product_details:
+                item_total = product_details.price * item.quantity
+                detailed_items.append({
+                    "product": product_details.model_dump(),
+                    "quantity": item.quantity,
+                    "item_total": round(item_total, 2),
+                })
+                total_price += item_total
+
+        return {
+            "message": "Cart products fetched successfully",
+            "data": {
+                "user_id": str(cart.user_id),
+                "total_price": round(total_price, 2),
+                "items": detailed_items,
+            }
         }
 
 cart_service = CartService()
