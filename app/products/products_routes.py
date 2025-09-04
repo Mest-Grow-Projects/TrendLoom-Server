b7844c5a9e7027205dad45bbd35d7f53cdc30e7c
george-asiedu <asiedug41@gmail.com>
2025-09-04 04:46:29 +0000

    route response models added

--- app/products/products_routes.py
+++ app/products/products_routes.py
@@ -1,6 +1,12 @@
 from fastapi import APIRouter, status
 from .products_service import product_service
-from app.schemas.products_schema import ProductsSchema, UpdateProductSchema
+from app.schemas.products_schema import (
+    ProductsSchema,
+    UpdateProductSchema,
+    ProductResponse,
+    ProductsResponse,
+    MessageResponse
+)
 
 router = APIRouter(
     prefix="/products",
@@ -11,6 +17,7 @@ router = APIRouter(
     "/create",
     status_code=status.HTTP_201_CREATED,
     summary="Create a new product",
+    response_model=ProductResponse,
 )
 async def create_product(data: ProductsSchema):
     return await product_service.create_product(data)
@@ -19,6 +26,7 @@ async def create_product(data: ProductsSchema):
     "/all",
     status_code=status.HTTP_200_OK,
     summary="Get all products",
+    response_model=ProductsResponse,
 )
 async def get_all_products():
     return await product_service.get_all_products()
@@ -27,6 +35,7 @@ async def get_all_products():
     "/{product_id}",
     status_code=status.HTTP_200_OK,
     summary="Get a product",
+    response_model=ProductResponse,
 )
 async def get_product(product_id: str):
     return await product_service.get_product_by_id(product_id)
@@ -35,6 +44,7 @@ async def get_product(product_id: str):
     "/{product_id}",
     status_code=status.HTTP_200_OK,
     summary="Update a product",
+    response_model=ProductResponse,
 )
 async def update_product(product_id: str, product: UpdateProductSchema):
     return await product_service.update_product_by_id(product_id, product)
@@ -43,6 +53,7 @@ async def update_product(product_id: str, product: UpdateProductSchema):
     "/{product_id}",
     status_code=status.HTTP_200_OK,
     summary="Delete a product",
+    response_model=MessageResponse,
 )
 async def delete_product(product_id: str):
     return await product_service.delete_product(product_id)
