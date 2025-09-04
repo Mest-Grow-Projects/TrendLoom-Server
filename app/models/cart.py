ecdc1f32e5fcfffd18ae8eaa761dcdba8d788bea
george-asiedu <asiedug41@gmail.com>
2025-09-04 09:34:32 +0000

    cart model defined

---
+++ app/models/cart.py
@@ -0,0 +1,17 @@
+from typing import List
+from beanie import Document
+from beanie import PydanticObjectId
+from pydantic import BaseModel
+from app.models.base_mixin import TimestampMixin
+
+
+class CartItem(BaseModel):
+    product_id: PydanticObjectId
+    quantity: int = 1
+
+class Cart(Document, TimestampMixin):
+    user_id: PydanticObjectId
+    items: List[CartItem] = []
+
+    class Settings:
+        name = 'cart'
