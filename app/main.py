4bf49f9def95b3b18a0218f925f4841b6ddfddd9
george-asiedu <asiedug41@gmail.com>
2025-09-11 16:08:48 +0000

    authenticated user guard added to routes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+from app.core.security.auth_guard import get_authenticated_user
 from app.database.db import init_db
-from app.core.constants import messages, origins
-from app.core.logging_config import logger
+from app.core.config.constants import messages, origins
+from app.core.config.logging_config import logger
 from app.auth.auth_route import router as auth_router
 from app.products.products_routes import router as products_router
 from app.users.users_route import router as users_router
@@ -43,6 +44,18 @@ def read_root():
     return messages["welcome"]
 
 app.include_router(auth_router, prefix="/api")
-app.include_router(products_router, prefix="/api")
-app.include_router(users_router, prefix="/api")
-app.include_router(cart_router, prefix="/api")
+app.include_router(
+    products_router,
+    prefix="/api",
+    dependencies=[Depends(get_authenticated_user)]
+)
+app.include_router(
+    users_router,
+    prefix="/api",
+    dependencies=[Depends(get_authenticated_user)]
+)
+app.include_router(
+    cart_router,
+    prefix="/api",
+    dependencies=[Depends(get_authenticated_user)]
+)
