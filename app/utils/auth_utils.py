e6f4f078598b8308338ebb5fef9689752c7821dc
george-asiedu <asiedug41@gmail.com>
2025-09-11 16:10:12 +0000

    jwt implemented

--- app/utils/auth_utils.py
+++ app/utils/auth_utils.py
@@ -1,74 +1,53 @@
 import random
 from datetime import timedelta, datetime, timezone
-from typing import Annotated
-from fastapi import Depends, HTTPException, status
+from typing import Dict
 from passlib.context import CryptContext
 import jwt
-from jwt.exceptions import InvalidTokenError
-from app.core.config import get_settings
+from app.core.config.config import get_settings
 from fastapi.security import OAuth2PasswordBearer
-from app.core.constants import status_messages
-from app.schemas.auth_schema import TokenData
-from app.database.repo.user_repo import find_user_by_id
-from app.models.user import User
+from app.schemas.auth_schema import AuthToken, UserResponse
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-SECRET_KEY = get_settings().SECRET_KEY
-ALGORITHM = get_settings().ALGORITHM
-access_token_expire = get_settings().ACCESS_TOKEN_EXPIRE_MINUTES
-refresh_token_expire = get_settings().REFRESH_TOKEN_EXPIRE_DAYS
+JWT_SECRET = get_settings().JWT_SECRET
+JWT_ALGORITHM = get_settings().JWT_ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
+def sign_jwt(payload: dict) -> str:
+    token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+    return token
 
 
-def verify_password(plain_password, hashed_password) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+def decode_jwt(token: str) -> dict | None:
+    return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
 
 
-def get_password_hash(password) -> str:
-    return pwd_context.hash(password)
+def create_login_tokens(user_data: UserResponse) -> Dict[str, str]:
+    access_token_expires = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    refresh_token_expires = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
 
+    access_payload = user_data.model_dump()
+    access_payload["id"] = str(access_payload["id"])
+    access_payload.update({
+        "exp": access_token_expires,
+        "token_type": AuthToken.ACCESS.value
+    })
+    access_token = sign_jwt(access_payload)
 
-def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=access_token_expire)
+    refresh_payload = {
+        "sub": str(user_data.id),
+        "exp": refresh_token_expires,
+        "token_type": AuthToken.REFRESH_TOKEN.value
+    }
+    refresh_token = sign_jwt(refresh_payload)
 
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token
+    }
 
-
-def create_refresh_token(data: dict) -> str:
-    expires = timedelta(days=refresh_token_expire)
-    return create_access_token(data, expires_delta=expires)
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]) -> User:
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail=status_messages["credentials_error"],
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub")
-        if user_id is None:
-            raise credentials_exception
-        token_data = TokenData(sub=user_id)
-    except InvalidTokenError:
-        raise credentials_exception
-
-    user = await find_user_by_id(token_data.sub)
-    if user is None:
-        raise credentials_exception
-    return user
-
-def password_match(password: str, confirm_password: str) -> bool:
-    return password == confirm_password
 
 def generate_verification_code() -> str:
     return str(random.randint(100000, 999999))
