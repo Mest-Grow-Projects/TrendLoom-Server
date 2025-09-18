16ceecc6cef975a9bb93016e8e171863316980cb
george-asiedu <asiedug41@gmail.com>
2025-09-18 18:06:02 +0000

    product model modified to include brand, category, tags, createdBy, and stock attributes

--- app/database/models/products.py
+++ app/database/models/products.py
@@ -1,9 +1,12 @@
 from typing import List, Annotated
 from enum import Enum
-from beanie import Document, Indexed
+from beanie import Document, Indexed, Link
 from pydantic import Field, BaseModel
 import pymongo
 from app.database.models.base_mixin import TimestampMixin
+from app.database.models.user import User
+from .category import Category
+from .brands import Brand
 
 
 class StockStatus(str, Enum):
@@ -16,22 +19,32 @@ class Attributes(BaseModel):
     size: List[str]
 
 class Product(Document, TimestampMixin):
-    name: str | None = None
-    description: str | None = None
-    price: Annotated[float, Indexed()] | None = None
+    name: str
+    description: str
+    price: Annotated[float, Indexed()]
     currency: str = Field(default="USD")
-    images: List[str] | None = None
+    image: List[str]
+    detailsImage: List[str] | None = None
+    brand: Link[Brand] | None = None
+    category: Link[Category] | None = None
     attributes: List[Attributes] | None = None
     features: List[str] | None = None
     specifications: List[str] | None = None
+    tags: List[str] | None = None
+    stock: int = Field(ge=0, default=0)
     stockStatus: StockStatus | None = StockStatus.IN_STOCK
     is_featured: bool | None = None
     is_new: bool | None = None
     rating: float | None = None
+    createdBy: Link[User]
 
     class Settings:
         name = "products"
         indexes = [
+            [("createdBy", pymongo.ASCENDING)],
+            [("brand", pymongo.ASCENDING)],
+            [("tags", pymongo.ASCENDING)],
+            [("category", pymongo.ASCENDING)],
             [("stockStatus", pymongo.ASCENDING)],
             [("is_featured", pymongo.ASCENDING)],
             [("is_new", pymongo.ASCENDING)],
