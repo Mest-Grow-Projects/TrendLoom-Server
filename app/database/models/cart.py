58a40104b206772e9d6a2b76e96f415c9d445851
george-asiedu <asiedug41@gmail.com>
2025-09-18 18:39:25 +0000

    cart model modified to link products and users models

--- app/database/models/cart.py
+++ app/database/models/cart.py
@@ -1,16 +1,17 @@
 from typing import List
-from beanie import Document
-from beanie import PydanticObjectId
+from beanie import Document, Link
 from pydantic import BaseModel
 from app.database.models.base_mixin import TimestampMixin
+from .user import User
+from .products import Product
 
 
 class CartItem(BaseModel):
-    product_id: PydanticObjectId
+    product_id: Link[Product]
     quantity: int = 1
 
 class Cart(TimestampMixin, Document):
-    user_id: PydanticObjectId
+    user_id: Link[User]
     items: List[CartItem] = []
 
     class Settings:
